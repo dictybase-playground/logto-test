7d170d97149e8b2822e21bd89d7ee516fb0efab3
Kevin Tun <kevin.tun@northwestern.edu>
2023-12-07 10:35:09 -0600

    private routing

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,14 +1,23 @@
-import { Stack, Typography } from "@mui/material";
+import { Stack, Typography, IconButton } from "@mui/material";
+import PersonIcon from '@mui/icons-material/Person';
 import { LogInOutButton } from "./LogInOutButton";
 import { Link } from "react-router-dom";
 
 const Header = () => {
+  console.log("header")
   return (
     <Stack direction="row" justifyContent="space-between">
       <Link to="/">
         <Typography variant="h3"> Auth App </Typography>
       </Link>
-      <LogInOutButton />
+      <Stack direction="row" alignItems="baseline">
+        <Link to="/user">
+          <IconButton>
+            <PersonIcon sx={{ color: "white" }}/>
+          </IconButton>
+        </Link>
+        <LogInOutButton />
+      </Stack>
     </Stack>
     )
 }
---
+++ src/pages/user.tsx
@@ -0,0 +1,7 @@
+import { User } from "../User"
+import { ACCESS } from "../constants"
+
+const access = ACCESS.private
+
+export { access }
+export default User
